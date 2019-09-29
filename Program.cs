e9f576cf4f857a45ad1de1b755448b3a0afcc8f7
Cripticpiramide <beau.sas@student.howest.be>
2019-09-30 00:04:40 +0200

    Initial commit (eerste commit)

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Currency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+            
+        }
+        static void CurrencyProgram()
+        {
+
+            int platToGold = 10;
+            int goldToSilver = 100;
+            int silverToCopper = 1000;
+
+            int goldToCopper = goldToSilver * silverToCopper;
+            int platToCopper = platToGold * goldToCopper;
+
+            Console.Write("Please fill in your amount of coppers: ");
+            int originalTotalAmountOfCoppers = int.Parse(Console.ReadLine());
+
+            int platDivision = originalTotalAmountOfCoppers / platToCopper;
+            int goldDivision = originalTotalAmountOfCoppers / goldToCopper;
+            int silverDivision = originalTotalAmountOfCoppers / silverToCopper;
+            int copperDivision = originalTotalAmountOfCoppers % 1000;
+
+            Console.Write("Your currency contains: " + platDivision + "p ");
+            Console.Write("Your currency contains: " + goldDivision + "g ");
+            Console.Write("Your currency contains: " + silverDivision + "s ");
+            Console.Write("Your currency contains: " + copperDivision + "c");
+
+
+            CurrencyProgram();
+
+            Console.ReadLine();
+
+        }
+    }
+}
